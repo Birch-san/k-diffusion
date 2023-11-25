230b5cd44cf57df7daca0a9ec7a191e1a7d234f9
Alex Birch <Birch-san@users.noreply.github.com>
2023-11-25 18:02:49 +0000

    give up on trying to migrate optimizer state if config changes. it is hard.

--- kdiff_trainer/migrations/migrate_model.py
+++ kdiff_trainer/migrations/migrate_model.py
@@ -2,12 +2,13 @@ from typing import Dict
 from torch.nn import Module
 from torch.optim import Optimizer
 from k_diffusion.models import ImageTransformerDenoiserModelV2
-from .migrate_image_transformer_v2 import register_load_hooks_vit_v2, forge_opt_state_vit_v2
+from .migrate_image_transformer_v2 import register_load_hooks_vit_v2, should_discard_optim_state_vit_v2
 
 def register_load_hooks(model: Module, orig_model_config: Dict, new_model_config: Dict) -> None:
   if isinstance(model, ImageTransformerDenoiserModelV2):
     register_load_hooks_vit_v2(model, orig_model_config, new_model_config)
 
-def forge_opt_state(model: Module, optim: Optimizer, step: int, new_opt_state_dict: Dict, orig_model_config: Dict, new_model_config: Dict) -> None:
+def should_discard_optim_state(model: Module, orig_model_config: Dict, new_model_config: Dict) -> bool:
   if isinstance(model, ImageTransformerDenoiserModelV2):
-    forge_opt_state_vit_v2(model, optim, step, new_opt_state_dict, orig_model_config, new_model_config)
+    return should_discard_optim_state_vit_v2(model, orig_model_config, new_model_config)
+  return False
