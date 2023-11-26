25fbfe0ffe08101da9fe4b3797723cd6502de3d1
Alex Birch <Birch-san@users.noreply.github.com>
2023-11-26 17:44:01 +0000

    add unet wrapper, schedule and sampler for consistency decoder

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -156,6 +156,23 @@ def sample_euler_ancestral(model, x, sigmas, extra_args=None, callback=None, dis
 
 
 @torch.no_grad()
+def sample_consistency(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None):
+    """Ancestral sampling with Euler method steps."""
+    extra_args = {} if extra_args is None else extra_args
+    noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        denoised = model(x, sigmas[i] * s_in, **extra_args)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
+        if sigmas[i + 1] > 0:
+            x = denoised + noise_sampler(sigmas[i], sigmas[i + 1]) * sigmas[i + 1]
+        else:
+            x = denoised
+    return x
+
+
+@torch.no_grad()
 def sample_heun(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
     """Implements Algorithm 2 (Heun steps) from Karras et al. (2022)."""
     extra_args = {} if extra_args is None else extra_args
