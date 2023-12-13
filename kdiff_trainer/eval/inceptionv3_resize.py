70a70c733724ac0fc8499792ae9be0cbcd18c1d0
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-13 18:10:07 +0000

    simplify

--- kdiff_trainer/eval/inceptionv3_resize.py
+++ kdiff_trainer/eval/inceptionv3_resize.py
@@ -13,16 +13,8 @@ class InceptionV3Resize(Module, Resize):
     batch_size, channels, height, width = img.shape
     assert channels == 3
     theta = torch.eye(2, 3, device=img.device)
-    _3 = torch.select(torch.select(theta, 0, 0), 0, 2)
-    _4 = torch.select(torch.select(theta, 0, 0), 0, 0)
-    _5 = torch.div(_4, width)
-    _6 = torch.select(torch.select(theta, 0, 0), 0, 0)
-    _3.add_(torch.sub(_5, torch.div(_6, 299)))
-    _8 = torch.select(torch.select(theta, 0, 1), 0, 2)
-    _9 = torch.select(torch.select(theta, 0, 1), 0, 1)
-    _10 = torch.div(_9, height)
-    _11 = torch.select(torch.select(theta, 0, 1), 0, 1)
-    _8.add_(torch.sub(_10, torch.div(_11, 299)))
+    theta[0,2] += 1/width - 1/299
+    theta[1,2] += 1/height - 1/299
     _13 = torch.unsqueeze(theta.to(img.dtype), 0)
     theta0 = _13.repeat([batch_size, 1, 1])
     grid = affine_grid(theta0, [batch_size, channels, 299, 299], False)
