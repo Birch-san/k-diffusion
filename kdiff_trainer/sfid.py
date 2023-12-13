38417de937f7921d2910c718909709b5f23deae3
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-13 17:23:54 +0000

    fix sFID, probably

--- kdiff_trainer/sfid.py
+++ kdiff_trainer/sfid.py
@@ -2,42 +2,16 @@ import torch
 from cleanfid.inception_torchscript import InceptionV3W
 from torch import Tensor, FloatTensor
 from torch.nn import Module, Sequential
-from torch.nn.functional import affine_grid, grid_sample, interpolate#, linear, softmax
+from torch.nn.functional import affine_grid, grid_sample, interpolate
 from typing import Tuple
 
-class Contiguous(Module):
-  def forward(self, x: Tensor) -> Tensor:
-    return x.contiguous()
-
-class TruncatedMixed6(Module):
-  layers: Sequential
-  def __init__(self, mixed6: Module) -> None:
-    super().__init__()
-    contig = Contiguous()
-    self.layers = Sequential(
-      mixed6.conv,
-      contig,
-      # for some reason whatever comes out of contig, has the wrong number of channels for tower
-      mixed6.tower,
-      contig,
-      mixed6.tower_1,
-      contig,
-      mixed6.tower_2,
-      contig,
-      # but we skip the final operation, cat(1)
-    )
-  def forward(self, x: Tensor) -> FloatTensor:
-    y: FloatTensor = self.layers.forward(x)
-    return y
-
 class SFID(Module):
-  resize_inside: bool
+  do_resize: bool
   layers: Sequential
-  def __init__(self, inception: InceptionV3W, resize_inside=False) -> None:
+  def __init__(self, inception: InceptionV3W, do_resize=False) -> None:
     super().__init__()
-    self.resize_inside = resize_inside
+    self.do_resize = do_resize
     layers: Module = inception.base.layers
-    mixed6 = TruncatedMixed6(layers.mixed_6)
     self.layers = Sequential(
       layers.conv,
       layers.conv_1,
@@ -52,15 +26,13 @@ class SFID(Module):
       layers.mixed_3,
       layers.mixed_4,
       layers.mixed_5,
-      mixed6,
+      layers.mixed_6.conv,
     )
 
-  def _resizey_forward(
+  def _resize(
     self,
     img: Tensor,
-    # return_features: bool=False,
     use_fp16: bool=False,
-    # no_output_bias: bool=False
   ) -> Tensor:
     batch_size, channels, height, width = img.shape
     assert torch.eq(channels, 3)
@@ -82,40 +54,23 @@ class SFID(Module):
     x0 = grid_sample(x, grid, "bilinear", "border", False)
     x1 = x0.sub_(128)
     x2 = x1.div_(128)
-    x3: FloatTensor = self.layers.forward(x2)
-    return x3.to(torch.float32)
-    # _14 = torch.reshape(self.layers.forward(x2), [-1, 2048])
-    # features = torch.to(_14, 6)
-    # if return_features:
-    #   _15 = features
-    # else:
-    #   if no_output_bias:
-    #     output = self.output
-    #     weight = output.weight
-    #     logits0 = linear(features, weight, None, )
-    #     logits = logits0
-    #   else:
-    #     output0 = self.output
-    #     logits = (output0).forward(features, )
-    #   _16 = softmax(logits, 1, 3, None, )
-    #   _15 = _16
-    # return _15
+    return x2
 
   def forward(self, x: Tensor) -> FloatTensor:
-    # bs = x.shape[0]
-    if self.resize_inside:
-      features = self._resizey_forward(x, return_features=True)#.view((bs, 2048))
+    if self.do_resize:
+      x = self._resize(x)
     else:
-      # make sure it is resized already
       assert (x.shape[2] == 299) and (x.shape[3] == 299)
       # apply normalization
       x1 = x - 128
       x2 = x1 / 128
-      features = self.layers.forward(x2)#.view((bs, 2048))
-    features_7chan: FloatTensor = features[:,:7,:,:]
-    return features_7chan
+    features: FloatTensor = self.layers.forward(x2)
+    # we have a 17x17 feature map. taking the first 7 channels (7*17*17=2023)
+    # gives us a comparable size to the 2048 pool_3 feature vector.
+    features = features[:,:7,:,:].flatten(start_dim=1)
+    return features.float()
 
-class ResizeyFeatureExtractor(Module):
+class ResizeyFeatureExtractor(Module):#
   extractor: Module
   size: Tuple[int, int]
   def __init__(self, extractor: Module, size: Tuple[int, int] = (299, 299)) -> None:
