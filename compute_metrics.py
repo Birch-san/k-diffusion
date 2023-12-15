b076891f816aef2099a683f94125fd7c808378b9
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-15 23:56:36 +0000

    compute_metrics: 1. [perf/mem] pre-allocate GPU tensors to receive all features (as opposed to transferring-to-CPU each batch then concatting then transferring-to-GPU). 2. [design] some extractors (i.e. inception-score-*) only need preds, not targets. in this situation we can relax the requirement to specify a --config-target, and avoid constructing a target dataloader, allocating target tensors, and extracting target features.

--- compute_metrics.py
+++ compute_metrics.py
@@ -4,12 +4,13 @@ import k_diffusion as K
 from k_diffusion.evaluation import InceptionV3FeatureExtractor, CLIPFeatureExtractor, DINOv2FeatureExtractor
 from pathlib import Path
 import torch
-from torch import distributed as dist, multiprocessing as mp, FloatTensor, cat
+from torch import distributed as dist, multiprocessing as mp, FloatTensor, zeros
 from torch.nn import Sequential
 from torch.nn.functional import kl_div
 from torch.utils import data
+from torch.utils.data.dataset import Dataset, IterableDataset
 from torchvision import transforms
-from typing import Dict, Literal, Callable, List, Iterator, Union, Set
+from typing import Dict, Literal, Callable, List, Iterator, Union, Set, TypedDict, Optional, Any, NamedTuple
 from tqdm import tqdm
 import math
 from os.path import dirname
@@ -29,6 +30,29 @@ from kdiff_trainer.normalize import Normalize_
 ExtractorName = Literal['inception', 'clip', 'dinov2', 'sfid-bicubic', 'sfid-bilinear', 'inception-score-bicubic', 'inception-score-bilinear']
 ExtractorType = Callable[[FloatTensor], FloatTensor]
 
+class FeaturesBySubject(TypedDict):
+    pred: FloatTensor
+    # inception score doesn't require targets
+    target: Optional[FloatTensor]
+
+class DatasetConfigAndPath(NamedTuple):
+    dataset_config: Dict[str, Any]
+    config_path: str
+
+inception_feature_size=2048
+sfid_feature_size=2023
+extractor_feature_size: Dict[ExtractorName, int] = {
+    'inception': inception_feature_size,
+    'clip': 512,
+    'dinov2': 1024,
+    'sfid-bicubic': sfid_feature_size,
+    'sfid-bilinear': sfid_feature_size,
+    'inception-score-bicubic': inception_feature_size,
+    'inception-score-bilinear': inception_feature_size,
+}
+does_extractor_need_targets: Callable[[str], bool]  = lambda extractor: not extractor.startswith('inception-score')
+extractor_target_needs: Dict[ExtractorName, int] = { extractor: does_extractor_need_targets(extractor) for extractor in extractor_feature_size.keys() }
+
 def ensure_distributed():
     if not dist.is_initialized():
         dist.init_process_group(world_size=1, rank=0, store=dist.HashStore())
@@ -40,7 +64,7 @@ def main():
                    help='the batch size')
     p.add_argument('--config-pred', type=str, required=True,
                    help='configuration file detailing a dataset of predictions from a model')
-    p.add_argument('--config-target', type=str, required=True,
+    p.add_argument('--config-target', type=str, required=False,
                    help='configuration file detailing a dataset of ground-truth examples')
     p.add_argument('--torchmetrics-fid', action='store_true',
                    help='whether to use torchmetrics FID (in addition to CleanFID)')
@@ -84,15 +108,17 @@ def main():
         seeds = torch.randint(-2 ** 63, 2 ** 63 - 1, [accelerator.num_processes], generator=torch.Generator().manual_seed(args.seed))
         torch.manual_seed(seeds[accelerator.process_index])
 
+    any_needs_targets: bool = any((extractor_target_needs[extractor] for extractor in args.evaluate_with))
+    if any_needs_targets:
+        if args.config_target is None:
+            raise ValueError('--config-target must be specified because one of the evaluators specified in --evaluate-with compares predictions to targets')
+
     config_pred, config_target = (K.config.load_config(config, use_json5=config.endswith('.jsonc')) for config in (args.config_pred, args.config_target))
     model_config = config_pred['model']
-    pred_dataset_config = config_pred['dataset']
 
     # TODO: allow non-square input sizes
     assert len(model_config['input_size']) == 2 and model_config['input_size'][0] == model_config['input_size'][1]
     size = model_config['input_size']
-
-    target_dataset_config = config_target['dataset']
 
     tf = transforms.Compose([
         transforms.Resize(size[0], interpolation=transforms.InterpolationMode.BICUBIC, antialias=True),
@@ -100,28 +126,36 @@ def main():
         transforms.ToTensor(),
     ])
 
-    pred_train_set, target_train_set = (
-        get_dataset(
-            dataset_config,
-            config_dir=Path(config_path).parent,
-            uses_crossattn=False,
-            tf=tf,
-            class_captions=None,
-        ) for dataset_config, config_path in (
-            (pred_dataset_config, args.config_pred),
-            (target_dataset_config, args.config_target),
-        )
-    )
+    dataset_config_and_paths: List[DatasetConfigAndPath] = []
+    dataset_config_and_paths.append(DatasetConfigAndPath(config_pred['dataset'], args.config_pred))
+    if any_needs_targets:
+        dataset_config_and_paths.append(DatasetConfigAndPath(config_target['dataset'], args.config_target))
+    datasets: List[Union[Dataset, IterableDataset]] = [get_dataset(
+        dataset_config,
+        config_dir=Path(config_path).parent,
+        uses_crossattn=False,
+        tf=tf,
+        class_captions=None,
+    ) for dataset_config, config_path in dataset_config_and_paths]
 
     if accelerator.is_main_process:
-        for set_name, train_set in zip(('pred', 'target'), (pred_train_set, target_train_set)):
+        for set_name, train_set in zip(('pred', *('target',)*any_needs_targets), datasets):
             try:
                 print(f'Number of items in {set_name} dataset: {len(train_set):,}')
             except TypeError:
                 pass
 
-    pred_train_dl, target_train_dl = (data.DataLoader(train_set, args.batch_size, shuffle=not isinstance(train_set, data.IterableDataset), drop_last=False, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set in (pred_train_set, target_train_set))
-    pred_train_dl, target_train_dl = accelerator.prepare(pred_train_dl, target_train_dl)
+    dls: List[data.DataLoader] = [data.DataLoader(train_set, args.batch_size, shuffle=not isinstance(train_set, data.IterableDataset), drop_last=False, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set in datasets]
+    dls = accelerator.prepare(*dls)
+    # if you give prepare() a list (n>1), it returns a Tuple[DataLoader, ...].
+    # if you give prepare() a list (n==1), it returns a Dataloader.
+    # it's an inconsistent contract. we tuple-ize it to compensate for that.
+    if not isinstance(dls, tuple):
+        dls = (dls,)
+
+    # taking an iterator is superfluous but gives us a type hint
+    iters: List[Iterator[List[FloatTensor]]] = [iter(dl) for dl in dls]
+    del datasets, dls
 
     @lru_cache(maxsize=1)
     def get_inception() -> InceptionV3FeatureExtractor:
@@ -175,11 +209,6 @@ def main():
     if args.compile:
         for extractor in extractors.values():
             torch.compile(extractor, fullgraph=True, mode='max-autotune')
-    
-    # taking an iterator is superfluous but gives us a type hint
-    pred_train_iter: Iterator[List[FloatTensor]] = iter(pred_train_dl)
-    target_train_iter: Iterator[List[FloatTensor]] = iter(target_train_dl)
-    del pred_train_dl, target_train_dl
 
     if args.torchmetrics_fid:
         from torchmetrics.image.fid import FrechetInceptionDistance
@@ -189,13 +218,19 @@ def main():
         fid_obj: FrechetInceptionDistance = accelerator.prepare(fid_obj)
         if args.compile:
             torch.compile(fid_obj, fullgraph=True, mode='max-autotune')
+    
     if accelerator.is_main_process:
-        features: Dict[ExtractorName, Dict[Literal['pred', 'target'], List[FloatTensor]]] = {
-            # TODO: we could actually pre-allocate a GPU tensor instead of accumulating a list of CPU tensors
-            extractor: { 'pred': [], 'target': [] } for extractor in extractors.keys()
-        }
-    # TODO: Inception score doesn't require target, so we could avoid requiring it in that case.. or at least avoid computing features
-    for subject, iter_ in zip(('pred', 'target'), (pred_train_iter, target_train_iter)):
+        features: Dict[ExtractorName, FeaturesBySubject] = {}
+        for extractor in extractors.keys():
+            feature_dim: int = extractor_feature_size[extractor]
+            needs_targets: bool = extractor_target_needs[extractor]
+            pred_features: FloatTensor = zeros([args.evaluate_n, feature_dim], device=device, dtype=torch.float32)
+            # inception-score doesn't need targets
+            target_features: Optional[FloatTensor] = zeros([args.evaluate_n, feature_dim], device=device, dtype=torch.float32) if needs_targets else None
+            features_by_subject_ = FeaturesBySubject(pred=pred_features, target=target_features)
+            features[extractor] = features_by_subject_
+
+    for subject, iter_ in zip(('pred', *('target',)*any_needs_targets), iters):
         # batch sizes from torch dataloader are not *necessarily* equal to the args.batch_size you requested!
         # dataloaders based on webdataset will give a smaller batch when they exhaust a .tar shard.
         total_samples = 0
@@ -215,11 +250,15 @@ def main():
                 # we may be forced to overshoot in order to give every process an equal batch size. we discard the excess.
                 samples_kept: int = min(curr_batch_per_proc * accelerator.num_processes, samples_remaining)
                 for extractor_name, extractor in extractors.items():
+                    if subject == 'target':
+                        needs_targets: bool = extractor_target_needs[extractor_name]
+                        if not needs_targets:
+                            break
                     extracted: FloatTensor = extractor(samples)
                     extracted = accelerator.gather(extracted)
                     if accelerator.is_main_process:
                         extracted = extracted[:samples_kept]
-                        features[extractor_name][subject].append(extracted.cpu())
+                        features[extractor_name][subject][total_samples:total_samples+extracted.shape[0]] = extracted
                     del extracted
                 if args.torchmetrics_fid:
                     fid_obj.update(samples, subject == 'target')
@@ -231,7 +270,7 @@ def main():
                     break
             else:
                 raise RuntimeError(f'Exhausted iterator before reaching {args.evaluate_n} {subject} samples. Only got {total_samples}')
-    del pred_train_iter, target_train_iter, extractors
+    del iters, extractors
     gc.collect()
 
     receipt_lines: List[str] = [args.result_description]
@@ -251,19 +290,9 @@ def main():
         del fid_obj, tm_fid
         gc.collect()
     if accelerator.is_main_process:
-        # using new variable name rather than reassigning, to get type inference to use the new type
-        features_: Dict[ExtractorName, Dict[Literal['pred', 'target'], FloatTensor]] = {
-            extractor_name: {
-                subject: cat(features_list) for subject, features_list in features_by_subject.items()
-            } for extractor_name, features_by_subject in features.items()
-        }
-        del features
-        gc.collect()
         wants_entropy_comparison: Set[str] = { 'inception-score-bicubic', 'inception-score-bilinear' }
-        for extractor_name, features_by_subject in features_.items():
-            pred, target = features_by_subject['pred'], features_by_subject['target']
-            pred = pred.to(device)
-            target = target.to(device)
+        for extractor_name, features_by_subject in features.items():
+            pred: FloatTensor = features_by_subject['pred']
             initial: Literal['I', 'C', 'D'] = extractor_name[0].upper()
             print(add_to_receipt(f'{extractor_name}, {pred.shape[0]} samples:'))
             if extractor_name in wants_entropy_comparison:
@@ -274,13 +303,15 @@ def main():
                 # kl_div(pred_mean, pred, reduction='none')
                 pass
             else:
+                target: FloatTensor = features_by_subject['target']
                 fid: FloatTensor = K.evaluation.fid(pred, target)
                 kid: FloatTensor = K.evaluation.kid(pred, target)
                 print(add_to_receipt(f'  F{initial}D: {fid.item():g}'))
                 print(add_to_receipt(f'  K{initial}D: {kid.item():g}'))
+                assert pred.shape[0] == target.shape[0], f"somehow we have a mismatch between number of ground-truth samples ({target.shape[0]}) and model-predicted samples ({pred.shape[0]})."
+                del target
             assert pred.shape[0] == args.evaluate_n, f"you requested --evaluate-n={args.evaluate_n}, but we found {pred.shape[0]} samples. perhaps the final batch was skipped due to rounding problems. try ensuring that evaluate_n is divisible by batch_size*procs without a remainder, or try simplifying the multi-processing (i.e. single-node or single-GPU)."
-            assert pred.shape[0] == target.shape[0], f"somehow we have a mismatch between number of ground-truth samples ({target.shape[0]}) and model-predicted samples ({pred.shape[0]})."
-            del pred, target
+            del pred
             gc.collect()
 
         if args.result_out_file is not None:
