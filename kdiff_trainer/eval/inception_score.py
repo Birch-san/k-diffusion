6bc1ab995a5ad2d7245cfafce19327cf587d94ed
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-16 20:05:19 +0000

    implement inception score output projection and splits. avoid double-resize and double-normalize. re-use instance.

--- kdiff_trainer/eval/inception_score.py
+++ kdiff_trainer/eval/inception_score.py
@@ -1,14 +1,17 @@
 import torch
 from torch import Tensor, FloatTensor
 from torch.nn import Module
+from torch.nn.functional import linear
 
 class InceptionSoftmax(Module):
   inception: Module
   use_fp16: bool
-  def __init__(self, inception_torchscript: Module, use_fp16=False) -> None:
+  output_bias: bool
+  def __init__(self, inception_torchscript: Module, use_fp16=False, output_bias=True) -> None:
     super().__init__()
     self.inception = inception_torchscript
     self.use_fp16 = use_fp16
+    self.output_bias = output_bias
 
   def forward(self, x: Tensor) -> FloatTensor:
     _, _, h, w = x.shape
@@ -16,6 +19,11 @@ class InceptionSoftmax(Module):
     if self.use_fp16:
       x = x.to(torch.float16)
     # TODO: check whether this gives us pool3:0 tensors
-    features: FloatTensor = self.inception(x).float()
-    probabilities: FloatTensor = features.softmax(-1)
+    features: FloatTensor = self.inception.layers(x)
+    features = features.flatten(start_dim=1).float()
+    if self.output_bias:
+      logits: FloatTensor = self.inception.model.base.output.forward(features)
+    else:
+      logits: FloatTensor = linear(features, self.inception.model.base.output.weight)
+    probabilities: FloatTensor = logits.softmax(-1)
     return probabilities
