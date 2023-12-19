4b470e9d5fe3aae107fb21e7aa7addfaf0e39983
Alex Birch <Birch-san@users.noreply.github.com>
2023-12-19 22:45:25 +0000

    asymmetric eval. free more references. use inference mode for torchmetrics.

--- compute_metrics.py
+++ compute_metrics.py
@@ -10,7 +10,7 @@ from torch.nn.functional import kl_div
 from torch.utils import data
 from torch.utils.data.dataset import Dataset, IterableDataset
 from torchvision import transforms
-from typing import Dict, Literal, Callable, List, Iterator, Union, Set, TypedDict, Optional, Any, NamedTuple
+from typing import Dict, Literal, Callable, List, Iterator, Union, Set, TypedDict, Optional, Any, NamedTuple, Tuple
 from tqdm import tqdm
 import math
 from os.path import dirname
@@ -71,6 +71,8 @@ def main():
                    help='whether to torch.compile() the feature extractors')
     p.add_argument('--evaluate-n', type=int, default=2000,
                    help='the number of samples to draw to evaluate')
+    p.add_argument('--evaluate-n-reals', type=int, default=None,
+                   help='number of reals to draw (if different from --evaluate-n)')
     p.add_argument('--evaluate-with', type=str, nargs='+', default=['inception'],
                    choices=['inception', 'clip', 'dinov2', 'sfid-bicubic', 'sfid-bilinear', 'inception-score-bicubic', 'inception-score-bilinear'],
                    help='the feature extractor to use for evaluation')
@@ -96,6 +98,8 @@ def main():
                    help='preample to include on any result that is written to a file ()')
     p.add_argument('--result-out-file', type=str, default=None,
                    help='file into which to output result')
+    p.add_argument('--shuffle-fakes', default=True, action=argparse.BooleanOptionalAction)
+    p.add_argument('--shuffle-reals', default=True, action=argparse.BooleanOptionalAction)
     args = p.parse_args()
 
     mp.set_start_method(args.start_method)
@@ -104,6 +108,9 @@ def main():
     accelerator = accelerate.Accelerator(mixed_precision=args.mixed_precision)
     ensure_distributed()
     device = accelerator.device
+    if accelerator.num_processes > 1:
+        if accelerator.is_main_process:
+            print("!!!\n[WARNING] multi-GPU is known to exhaust the dataloader iterator without yielding all the expected samples, at least for wds datasets. An assertion will tell you if this happens. You may need to try single-GPU.\n!!!")
 
     if args.seed is not None:
         seeds = torch.randint(-2 ** 63, 2 ** 63 - 1, [accelerator.num_processes], generator=torch.Generator().manual_seed(args.seed))
@@ -136,13 +143,15 @@ def main():
     if any_needs_targets:
         _, config_target = configs
         dataset_config_and_paths.append(DatasetConfigAndPath(config_target['dataset'], args.config_target))
+    wants_shuffles: Tuple[bool, ...] = (args.shuffle_fakes, *(args.shuffle_reals,)*any_needs_targets)
     datasets: List[Union[Dataset, IterableDataset]] = [get_dataset(
         dataset_config,
         config_dir=Path(config_path).parent,
         uses_crossattn=False,
         tf=tf,
         class_captions=None,
-    ) for dataset_config, config_path in dataset_config_and_paths]
+        shuffle_wds=wants_shuffle,
+    ) for (dataset_config, config_path), wants_shuffle in zip(dataset_config_and_paths, wants_shuffles)]
 
     if accelerator.is_main_process:
         for set_name, train_set in zip(('pred', *('target',)*any_needs_targets), datasets):
@@ -151,7 +160,7 @@ def main():
             except TypeError:
                 pass
 
-    dls: List[data.DataLoader] = [data.DataLoader(train_set, args.batch_size, shuffle=not isinstance(train_set, data.IterableDataset), drop_last=False, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set in datasets]
+    dls: List[data.DataLoader] = [data.DataLoader(train_set, args.batch_size, shuffle=wants_shuffle and not isinstance(train_set, data.IterableDataset), drop_last=False, num_workers=args.num_workers, persistent_workers=True, pin_memory=True) for train_set, wants_shuffle in zip(datasets, wants_shuffles)]
     dls = accelerator.prepare(*dls)
     # if you give prepare() a list (n>1), it returns a Tuple[DataLoader, ...].
     # if you give prepare() a list (n==1), it returns a Dataloader.
@@ -229,31 +238,44 @@ def main():
         if args.compile:
             torch.compile(fid_obj, fullgraph=True, mode='max-autotune')
     
+    fakes_quota: int = args.evaluate_n
+    reals_quota: int = args.evaluate_n if args.evaluate_n_reals is None else args.evaluate_n_reals
+
+    if accelerator.is_main_process:
+        reals_msg = f' against {reals_quota} reals' if any_needs_targets else ''
+        quota_msg = f'Evaluating {fakes_quota} fakes{reals_msg}'
+        print(quota_msg)
+    
     if accelerator.is_main_process:
         features: Dict[ExtractorName, FeaturesBySubject] = {}
         for extractor in extractors.keys():
             feature_dim: int = extractor_feature_size[extractor]
             needs_targets: bool = extractor_target_needs[extractor]
-            pred_features: FloatTensor = zeros([args.evaluate_n, feature_dim], device=device, dtype=torch.float32)
+            pred_features: FloatTensor = zeros([fakes_quota, feature_dim], device=device, dtype=torch.float32)
             # inception-score doesn't need targets
-            target_features: Optional[FloatTensor] = zeros([args.evaluate_n, feature_dim], device=device, dtype=torch.float32) if needs_targets else None
+            target_features: Optional[FloatTensor] = zeros([reals_quota, feature_dim], device=device, dtype=torch.float32) if needs_targets else None
             features_by_subject_ = FeaturesBySubject(pred=pred_features, target=target_features)
             features[extractor] = features_by_subject_
 
-    for subject, iter_ in zip(('pred', *('target',)*any_needs_targets), iters):
+    for subject, iter_, quota in zip(
+        ('pred', *('target',)*any_needs_targets),
+        iters,
+        (fakes_quota, *(reals_quota,)*any_needs_targets),
+    ):
         # batch sizes from torch dataloader are not *necessarily* equal to the args.batch_size you requested!
         # dataloaders based on webdataset will give a smaller batch when they exhaust a .tar shard.
         total_samples = 0
         with tqdm(
             desc=f'Computing features for {subject}...',
-            total=args.evaluate_n,
+            total=quota,
             disable=not accelerator.is_main_process,
             position=0,
             unit='samp',
         ) as pbar:
             for batch in iter_:
                 samples, *_ = batch
-                samples_remaining: int = args.evaluate_n-total_samples
+                del batch
+                samples_remaining: int = quota-total_samples
                 # (optimization): use a smaller per-proc batch if possible (only relevant for the final batch)
                 curr_batch_per_proc: int = min(samples.shape[0], math.ceil(samples_remaining / accelerator.num_processes))
                 samples = samples[:curr_batch_per_proc]
@@ -272,15 +294,17 @@ def main():
                         features[extractor_name][subject][total_samples:total_samples+extracted.shape[0]] = extracted
                     del extracted
                 if args.torchmetrics_fid:
-                    fid_obj.update(samples, subject == 'target')
+                    with inference_mode():
+                        fid_obj.update(samples, subject == 'target')
+                del samples
                 total_samples += samples_kept
                 pbar.update(samples_kept)
-                assert total_samples <= args.evaluate_n
+                assert total_samples <= quota
                 gc.collect()
-                if total_samples == args.evaluate_n:
+                if total_samples == quota:
                     break
             else:
-                raise RuntimeError(f'Exhausted iterator before reaching {args.evaluate_n} {subject} samples. Only got {total_samples}')
+                raise RuntimeError(f'Exhausted iterator before reaching {quota} {subject} samples. Only got {total_samples}. If you are using multi-GPU: try, uh, not using multi-GPU.')
     del iters, extractors
     gc.collect()
 
@@ -294,10 +318,10 @@ def main():
         # FrechetInceptionDistance's superclass "Metric" wraps FrechetInceptionDistance#update and FrechetInceptionDistance#compute to support distributed operation
         tm_fid: FloatTensor = fid_obj.compute()
         if accelerator.is_main_process:
-            print(add_to_receipt(f'torchmetrics, {fid_obj.fake_features_num_samples} samples:'))
+            print(add_to_receipt(f'torchmetrics, {fid_obj.fake_features_num_samples} fakes vs {fid_obj.real_features_num_samples} reals:'))
             print(add_to_receipt(f'  FID: {tm_fid.item()}'))
-            assert fid_obj.fake_features_num_samples.item() == args.evaluate_n, f"[torchmetrics FID] you requested --evaluate-n={args.evaluate_n}, but we found {fid_obj.fake_features_num_samples} samples. perhaps the final batch was skipped due to rounding problems. try ensuring that evaluate_n is divisible by batch_size*procs without a remainder, or try simplifying the multi-processing (i.e. single-node or single-GPU)."
-            assert fid_obj.fake_features_num_samples.item() == fid_obj.real_features_num_samples.item(), f"[torchmetrics FID] somehow we have a mismatch between number of ground-truth samples ({fid_obj.real_features_num_samples}) and model-predicted samples ({fid_obj.fake_features_num_samples})."
+            assert fid_obj.fake_features_num_samples.item() == fakes_quota, f"[torchmetrics FID] you requested --evaluate-n={fakes_quota}, but we found {fid_obj.fake_features_num_samples} samples. perhaps the final batch was skipped due to rounding problems. try ensuring that evaluate_n is divisible by batch_size*procs without a remainder, or try simplifying the multi-processing (i.e. single-node or single-GPU)."
+            assert fid_obj.real_features_num_samples.item() == reals_quota, f"[torchmetrics FID] you requested --evaluate-n={reals_quota}, but we found {fid_obj.real_features_num_samples} samples. perhaps the final batch was skipped due to rounding problems. try ensuring that evaluate_n is divisible by batch_size*procs without a remainder, or try simplifying the multi-processing (i.e. single-node or single-GPU)."
         del fid_obj, tm_fid
         gc.collect()
     if accelerator.is_main_process:
@@ -330,9 +354,7 @@ def main():
                 kid: FloatTensor = K.evaluation.kid(pred, target)
                 print(add_to_receipt(f'  F{initial}D: {fid.item():g}'))
                 print(add_to_receipt(f'  K{initial}D: {kid.item():g}'))
-                assert pred.shape[0] == target.shape[0], f"somehow we have a mismatch between number of ground-truth samples ({target.shape[0]}) and model-predicted samples ({pred.shape[0]})."
                 del target
-            assert pred.shape[0] == args.evaluate_n, f"you requested --evaluate-n={args.evaluate_n}, but we found {pred.shape[0]} samples. perhaps the final batch was skipped due to rounding problems. try ensuring that evaluate_n is divisible by batch_size*procs without a remainder, or try simplifying the multi-processing (i.e. single-node or single-GPU)."
             del pred
             gc.collect()
 
