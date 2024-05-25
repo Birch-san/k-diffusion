5b6de66142dcb3e0bdf78f07acecd9bda01b37f9
Alex Birch <Birch-san@users.noreply.github.com>
2024-05-25 14:09:30 +0100

    FFHQ ablation script

---
+++ shell/ffhq-rope-ablate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# run this from the root of the k-diffusion directory, with virtual env activated
+
+python train.py \
+--config \
+configs/ffhq/config_FFHQ_256px_rope.jsonc \
+--out-root out \
+--output-to-subdir \
+--name ffhq-256-rope \
+--evaluate-n 0 \
+--batch-size 40 \
+--demo-every 500 \
+--save-every 500 \
+--sample-n 36 \
+--mixed-precision bf16 \
+--demo-classcond-include-uncond \
+--demo-img-compress \
+--end-step 10000 \
+--font ./kdiff_trainer/font/DejaVuSansMono.ttf \
+--start-method fork \
+--wandb-project hdit-ffhq-256 \
+--wandb-entity mahouko \
+--wandb-run-name ffhq-256-rope
+
+python train.py \
+--config \
+configs/ffhq/config_FFHQ_256px_additive.jsonc \
+--out-root out \
+--output-to-subdir \
+--name ffhq-256-additive \
+--evaluate-n 0 \
+--batch-size 40 \
+--demo-every 500 \
+--save-every 500 \
+--sample-n 36 \
+--mixed-precision bf16 \
+--demo-classcond-include-uncond \
+--demo-img-compress \
+--end-step 10000 \
+--font ./kdiff_trainer/font/DejaVuSansMono.ttf \
+--start-method fork \
+--wandb-project hdit-ffhq-256 \
+--wandb-entity mahouko \
+--wandb-run-name ffhq-256-additive
